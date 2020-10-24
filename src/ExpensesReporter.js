a2ef7faeb3a96811c09e82c5b0c89d511412ecc1
Ulises Santana <ulisesantana@gmail.com>
2020-10-24 21:44:28 +0100

    use private properties

--- src/ExpensesReporter.js
+++ src/ExpensesReporter.js
@@ -1,49 +1,51 @@
 class ExpensesReporter {
+  #expenses = [];
+  #categories = {};
+  #categoriesKeys = [];
+  #emptyReport = {
+    total: 0,
+    expenses: {},
+  };
+
   constructor(categories, expenses) {
-    this.expenses = expenses;
-    this.categories = categories;
-    this.report = {
-      total: 0,
-      expenses: {},
-    };
+    this.#expenses = expenses;
+    this.#categories = categories;
+    this.#categoriesKeys = this.#mapCategoriesToPropertyKeys(this.#categories);
   }
 
   exec() {
-    return Boolean(this.expenses) && this.expenses.length
-      ? this.expenses.reduce(this.#expensesReducer(), this.report)
-      : this.report;
+    return Boolean(this.#expenses) && this.#expenses.length
+      ? this.#expenses.reduce(this.#expensesReducer, this.#emptyReport)
+      : this.#emptyReport;
   }
 
-  #expensesReducer = () => {
-    const categoriesKeys = this.#mapCategoriesToPropertyKeys(this.categories);
-    return (report, { description, date, amount, category }) => {
-      const categoryKey = categoriesKeys[category];
-      return {
-        total: this.#sum(report.total, amount),
-        expenses: {
-          ...report.expenses,
-          [categoryKey]: {
-            name: this.categories[category],
-            total: this.#sum(report.expenses[categoryKey]?.total || 0, amount),
-            detailedExpenses: (
-              report.expenses[categoryKey]?.detailedExpenses || []
-            ).concat({ description, date, amount }),
-          },
+  #expensesReducer = (report, { description, date, amount, category }) => {
+    const categoryKey = this.#categoriesKeys[category];
+    return {
+      total: this.#sum(report.total, amount),
+      expenses: {
+        ...report.expenses,
+        [categoryKey]: {
+          name: this.#categories[category],
+          total: this.#sum(report.expenses[categoryKey]?.total || 0, amount),
+          detailedExpenses: (
+            report.expenses[categoryKey]?.detailedExpenses || []
+          ).concat({ description, date, amount }),
         },
-      };
+      },
     };
   };
 
   #mapCategoriesToPropertyKeys = () => {
-    return Boolean(this.categories) && Object.keys(this.categories)
-      ? Object.entries(this.categories).reduce(
+    return Boolean(this.#categories) && Object.keys(this.#categories)
+      ? Object.entries(this.#categories).reduce(
           (acc, [key, value]) => ({
             ...acc,
             [key]: value.toLowerCase().replace(" ", "_"),
           }),
           {}
         )
-      : this.categories;
+      : this.#categories;
   };
 
   #sum = (...numsToSum) => {
